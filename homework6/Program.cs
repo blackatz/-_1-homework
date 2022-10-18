73b59aaed2a6bad7a75e39035387a83f94915d1a
blackatz <ksenya.noskova.98@inbox.ru>
2022-10-18 22:06:17 +0300

    homework

--- homework6/Program.cs
+++ homework6/Program.cs
@@ -1,2 +1,16 @@
 ﻿// напишите программу, которая на вход принимает число и выдает,
 // является ли число четным (делится ли оно на два без остатка)
+
+Console.WriteLine("Программа вычисления четного числа.");
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number%2 == 0)
+{
+    Console.WriteLine("Да");
+}
+
+else 
+{
+    Console.WriteLine("Нет");
+}
---
+++ homework8/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N
+
+Console.WriteLine("Программа вычисления всех четных чисел от 0 до N.");
+Console.WriteLine("Введите N: ");
+int N = int.Parse(Console.ReadLine()!);
+int N2 = 0;
+
+if (N < 0)
+    while (N2 >= N)
+    {
+    Console.Write(N2 + " ");
+    N2 = N2 - 2;
+    }
+
+else
+    while (N2 <= N)
+    { 
+        Console.Write(N2 + " ");
+        N2= N2 + 2;
+    }
