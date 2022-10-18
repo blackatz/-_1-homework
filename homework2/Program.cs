d41076d039fd367d153f5da3a3bd3f8e4500e232
blackatz <ksenya.noskova.98@inbox.ru>
2022-10-18 21:39:34 +0300

    homeworks

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -1,1 +1,17 @@
 ﻿// напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее
+
+Console.WriteLine("Программа вычисления наибольшего числа.");
+Console.WriteLine("Введите первое число: ");
+int input1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int input2 = int.Parse(Console.ReadLine()!);
+
+if (input1 < input2)
+{
+    Console.WriteLine(input2 + " больше");
+}
+
+if (input2 < input1)
+{
+    Console.WriteLine(input1 + " больше");
+}
---
+++ homework4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+//и выдает максимальное из этих чисел
+
+Console.WriteLine("Программа вычисления наибольшего числа.");
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = " + max);
