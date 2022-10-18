4be3dc949383f25c559a7da1fc6c0f5bd0cbc2a4
blackatz <ksenya.noskova.98@inbox.ru>
2022-10-18 22:43:31 +0300

    new

--- homework8/Program.cs
+++ homework8/Program.cs
@@ -1,16 +1,17 @@
 ﻿// Напишите программу, которая на вход принимает число (N), 
 // а на выходе показывает все чётные числа от 1 до N
 
-Console.WriteLine("Программа вычисления всех четных чисел от 0 до N.");
+Console.WriteLine("Программа вычисления всех четных чисел от 1 до N.");
 Console.WriteLine("Введите N: ");
 int N = int.Parse(Console.ReadLine()!);
-int N2 = 0;
+int N2 = 2;
+int N3 = -2;
 
 if (N < 0)
-    while (N2 >= N)
+    while (N3 >= N)
     {
-    Console.Write(N2 + " ");
-    N2 = N2 - 2;
+    Console.Write(N3 + " ");
+    N3 = N3 - 2;   
     }
 
 else
